9603189d2d9634d9434200ff741a516f6ceac01d
snowfxl <rexxalfaro+github@gmail.com>
2026-09-11 21:10:16 -0700

    refactor: integrate the new type for segment and change the return type of transcribe_video

--- video.py
+++ video.py
@@ -1,22 +1,52 @@
 import subprocess
+import sys
 from pathlib import Path
 from typing import Any
 
 import whisper
 
 from utils import format_timestamp
+from video_types import Segment, TranscriptionResult
 
 model = whisper.load_model("base")
 
 
-def transcribe_video(video_path: str) -> list[dict[str, Any]]:
-    result: dict[str, Any] = model.transcribe(video_path)
-    segments: list[dict[str, Any]] = result["segments"]
+def transcribe_video(video_path: str, language=None) -> TranscriptionResult:
+    segments: list[Segment] = []
+    error_message: str | None
 
-    return segments
+    try:
+        if language is None:
+            result: dict[str, Any] = model.transcribe(video_path)
+        else:
+            result: dict[str, Any] = model.transcribe(video_path, language=language)
+
+        segments = result["segments"]
+
+        error_message = None
+
+    except Exception as e:  # noqa: BLE001
+        print(
+            f"🛑 WARNING: Failed to transcribe '{video_path}'. Error details:",
+            file=sys.stderr,
+        )
+        print(e, file=sys.stderr)
+
+        error_message = (
+            f"Transcription failed due to an internal error: {type(e).__name__}."
+        )
+
+    finally:
+        pass
+
+    return {
+        "success": error_message is None,
+        "segments": segments,
+        "error_message": error_message,
+    }
 
 
-def create_srt(srt_path: Path, segments: list[dict[str, Any]]) -> None:
+def create_srt(srt_path: Path, segments: list[Segment]) -> None:
 
     with open(srt_path, "w", encoding="utf-8") as file:
         for i, segment in enumerate(segments):
